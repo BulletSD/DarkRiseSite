570db13dfa018970ba82927cd96586ba5109e8be
BulletSD <kozlov.vadim.leo@gmail.com>
2026-07-27 12:20:07 +0300

    config.php

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
 /**
-* @package   Catalyst
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
+ * @package   Catalyst
+ * @author    YOOtheme http://www.yootheme.com
+ * @copyright Copyright (C) YOOtheme GmbH
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
 
-require_once(dirname(__FILE__).'/warp/warp.php');
-
+// Подключаем ядро фреймворка WARP
+require_once( dirname( __FILE__ ) . '/warp/warp.php' );
 $warp = Warp::getInstance();
 
-// add paths
-$warp['path']->register(dirname(__FILE__).'/warp/systems/wordpress/helpers','helpers');
-$warp['path']->register(dirname(__FILE__).'/warp/systems/wordpress/layouts','layouts');    
-$warp['path']->register(dirname(__FILE__).'/layouts','layouts');    
-$warp['path']->register(dirname(__FILE__).'/js', 'js');
-$warp['path']->register(dirname(__FILE__).'/css', 'css');
+// Регистрируем пути к ресурсам темы
+$warp['path']->register( dirname( __FILE__ ) . '/warp/systems/wordpress/helpers', 'helpers' );
+$warp['path']->register( dirname( __FILE__ ) . '/warp/systems/wordpress/layouts', 'layouts' );
+$warp['path']->register( dirname( __FILE__ ) . '/layouts', 'layouts' );
+$warp['path']->register( dirname( __FILE__ ) . '/js', 'js' );
+$warp['path']->register( dirname( __FILE__ ) . '/css', 'css' );
 
-// init system
+// Инициализация системы темы
 $warp['system']->init();
