8285440885c5d6d5871d6146fb3fa900600164b2
BulletSD <kozlov.vadim.leo@gmail.com>
2026-07-27 12:24:09 +0300

    header.php

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <?php
 /**
-* @package   Catalyst
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
+ * @package   Catalyst
+ * @author    YOOtheme http://www.yootheme.com
+ * @copyright Copyright (C) YOOtheme GmbH
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
 
-// start output buffer to capture content for use in footer.php
+// Запускаем буферизацию вывода — контент страницы соберётся здесь,
+// а обёртка темы (шапка, меню, подвал) будет собрана в footer.php
 ob_start();
