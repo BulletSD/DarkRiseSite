e3428422265fa4200f3af69c452cc00727fc4495
BulletSD <kozlov.vadim.leo@gmail.com>
2026-07-27 12:17:42 +0300

    functions.php
    Updated comments and added a description in Russian for loading configuration.

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @package   Catalyst
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
+ * @package   Catalyst
+ * @author    YOOtheme http://www.yootheme.com
+ * @copyright Copyright (C) YOOtheme GmbH
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
 
-// load config    
-require_once(dirname(__FILE__).'/config.php');
+// Подключаем конфигурацию темы (движок WARP и настройки)
+require_once( dirname( __FILE__ ) . '/config.php' );
